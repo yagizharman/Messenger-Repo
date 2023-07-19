af427df636e34aaa8fe63ad0ea23369b16ff60ae
yagizharman <yagizharman4biz@gmail.com>
2023-07-19 23:38:51 +0300

    Userbox added to the design

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+
+    images: {
+        domains: [
+            "lh3.googleusercontent.com",
+            "avatars.githubusercontent.com",
+            "res.cloudinary.com"
+        ]
+    }
+}
 
 module.exports = nextConfig
