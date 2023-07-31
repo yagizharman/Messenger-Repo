c8bb2611ebcb78e5c4ba14e21fa43ad0877d25e9
yagizharman <yagizharman4biz@gmail.com>
2023-07-31 18:07:51 +0300

    deployment error fix.

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -5,10 +5,10 @@ const useConversation = () => {
     const params = useParams();
 
     const conversationId = useMemo(() => {
-        if (!params.conversationId) {
+        if (!params?.conversationId) {
             return '';
         }
-        return params.conversationId as string;
+        return params?.conversationId as string;
     }, [params?.conversationId])
 
     const isOpen = useMemo(() => !!conversationId, [conversationId])
